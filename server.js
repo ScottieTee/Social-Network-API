e0dd1330fee99b1e4752976b3c69ea63d23bcc55
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-07 21:08:45 -0400

    tweaked connection

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(require("./routes"));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Social-Network', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Connected on localhost:${PORT}`);
+  });
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-app.listen(PORT, ()=> console.log(`Connected on localhost:${PORT}`));
