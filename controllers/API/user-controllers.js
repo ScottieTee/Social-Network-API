a520c7baf4db75b59f05040655788d2f3b930c2b
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-13 16:58:50 -0400

    pesky bracket removed

--- controllers/API/user-controllers.js
+++ controllers/API/user-controllers.js
@@ -2,50 +2,49 @@ const res = require('express/lib/response');
 const User = require('../../models');
 
 const userControllers = {
-    async totalUsers(req, res) {
-      try {
-        const users = await User.find({});
-        if(!users.length) {
-            res.json({message: "No users found."})
-        }
-        return;
-      }
+  async totalUsers(req, res) {
+    try {
+      const users = await User.find({});
+      if (!users.length) {
+        res.json({ message: "No users found." })
+      return;
+    }
         res.json(users);
+  } catch(error) {
+    res.json(error);
+  }
+},
+
+  async newUser({ body }, res) {
+    try {
+      const newUser = await User.create(body);
+      if (!newUser) {
+        res.status(503).json({ message: "User not created." });
+      }
+      res.json(newUser);
     } catch (error) {
       res.json(error);
     }
-},
+  },
 
-async newUser({ body }, res) {
+    async getUserById({ params }, res) {
   try {
-    const newUser = await User.create(body);
-    if(!newUser) {
-        res.status(503).json({message: "User not created."});
-    }
-    res.json(newUser);
-} catch (error) {
-  res.json(error);
-}
-},
-
-async getUserById({ params }, res) {
-    try {
-      const userData = await User.findOne({ _id:params.id })
-        .populate('thoughts')
-        .populate('friends')
-        .select(["-_v", "-_id", "-email"]);
-    if(!userData) {
-        res.json({ message: "User not found." });
+    const userData = await User.findOne({ _id: params.id })
+      .populate('thoughts')
+      .populate('friends')
+      .select(["-_v", "-_id", "-email"]);
+    if (!userData) {
+      res.json({ message: "User not found." });
       return;
     }
     res.json(userData);
-} catch (error) {
-  res.json(error);
-}
+  } catch (error) {
+    res.json(error);
+  }
 },
 
 async updateUser({ params, body }, res) {
-    try {
+  try {
     const userData = await User.findOneAndUpdate({ _id: params.id }, body, {
       new: true,
     });
@@ -56,46 +55,46 @@ async updateUser({ params, body }, res) {
 },
 
   async deleteUser({ params }, res) {
-    try{
-      const userData = await User.findOneAndDelete(
+  try {
+    const userData = await User.findOneAndDelete(
       { _id: params.id },
       { new: true }
     );
 
-    if(!userData) {
-        res.json({message: "User not found."})
+    if (!userData) {
+      res.json({ message: "User not found." })
     }
 
     res.json(userData);
   } catch (error) {
     res.json(error);
   }
-  },
+},
 
   async addFriend({ params }, res) {
-      try{
-        const friendData = await User.findOne({ _id: params.friendId });
-        const userData = await User.findOneAndUpdate(
-          { _id: params.userId},
-          { $push: {friends: friendData._id } },
-        { new: true }
-      ).populate("friends");
+  try {
+    const friendData = await User.findOne({ _id: params.friendId });
+    const userData = await User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: friendData._id } },
+      { new: true }
+    ).populate("friends");
 
-      res.json(userData);
+    res.json(userData);
   } catch (error) {
     res.json(error);
   }
 },
 
 async removeFriend({ params }, res) {
-  try{
+  try {
     const userData = await User.findOneAndUpdate(
       { _id: params.userId },
-      { $pull: { friends: {$in: [params.friendId] } } },
+      { $pull: { friends: { $in: [params.friendId] } } },
       { new: true }
-      ).populate("friends");
-      await userData.save();
-      res.json(userData);
+    ).populate("friends");
+    await userData.save();
+    res.json(userData);
   } catch (error) {
     res.json(error);
   }
